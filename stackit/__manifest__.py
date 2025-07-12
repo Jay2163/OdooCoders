e8257e56ac150fcca9d7e94bd1de56f66a9a521f
Jay Butani <jaybutani606@gmail.com>
2025-07-12 10:32:36 +0530

    [ADD] Stackit | Setup of project

---
+++ stackit/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Stackit',
+    'version': '18.0.1.0.0',
+    'author': 'Jay Butani',
+    'sequence': 5,
+    'summary': 'Stackit Module',
+    'description': "StackIt is a minimal, user-friendly Q&A platform that enables collaborative "
+                   "learning and structured knowledge sharing through features "
+                   "like rich text editing, tagging, voting, and real-time "
+                   "notifications.",
+    'data': [
+    ],
+    'assets': {
+    },
+    'depends':['base'],
+    'application': True,
+    'license': 'LGPL-3',
+}
+
